dcaca22cf0c74682b2feff2b672e1ddb7d52f3d8
Brian Walsh <brian@bwalsh.com>
2024-10-07 18:04:01 -0700

    log unexpected results

--- image_viewer/object_signer.py
+++ image_viewer/object_signer.py
@@ -1,9 +1,19 @@
 from gen3.auth import Gen3Auth
 from gen3.file import Gen3File
+import logging
 
 
 def get_signed_url(object_id: str, access_token: str) -> str:
     """Call Gen3 to get a signed URL for the object_id"""
-    auth = Gen3Auth(access_token=access_token)
+
+    logger = logging.getLogger("uvicorn.error")
+
+    auth = Gen3Auth(refresh_file=f"accesstoken:///{access_token}")
+
     file = Gen3File(auth)
-    return file.get_presigned_url(object_id)['url']
+    file_info = file.get_presigned_url(object_id)
+
+    if 'url' not in file_info:
+        logger.error(file_info)
+
+    return file_info['url']
